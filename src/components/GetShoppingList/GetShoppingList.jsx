02b983e3e4212ed8ff8c48892d3448f26ecdfc5e
Samantha Marlowe <sam.c.marlowe@gmail.com>
2023-03-23 15:31:48 -0500

    started buy button

--- src/components/GetShoppingList/GetShoppingList.jsx
+++ src/components/GetShoppingList/GetShoppingList.jsx
@@ -1,10 +1,22 @@
 import { useState, useEffect } from 'react';
 
+const [toggle, setToggle] = useState(true);
+
 function GetShoppingList({list}) {
     console.log('in GetShoppingList', list)
 
     function buyBtnToggle(){
         console.log('inside buyBtnToggle()')
+        //if you hit button
+        //see text "purchased"
+        setToggle(!toggle);
+
+        if(toggle){
+            return
+            <div>
+                <p>Purchased!</p>
+            </div>
+        }
         
     }
 
@@ -23,8 +35,10 @@ function GetShoppingList({list}) {
             <li>{item.name}</li>
             <li>{item.quantity}</li>
             <li>{item.unit}</li>
+            <div id="buy">
             <button onClick={buyBtnToggle}>Buy</button>
             <button>Remove</button>
+            </div>
             </ul>
         </>
        
