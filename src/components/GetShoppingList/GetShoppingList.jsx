a46bcd2eb8403df6c5c534d455ef2e00cafeec8b
Siv Earley <searley96@gmail.com>
2023-03-23 13:25:52 -0500

    created get route on client side

---
+++ src/components/GetShoppingList/GetShoppingList.jsx
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react';
+
+function GetShoppingList({list}) {
+    console.log('in GetShoppingList', list)
+
+    return (
+       <>
+       <div>
+        <h2>Shopping List</h2>
+        <button>Reset</button>
+        <button>Clear</button>
+       </div>
+       <div>
+        {list.map(item => (
+        <ul key={item.id}>
+            <li>{item.name}</li>
+            <li>{item.quantity}</li>
+            <li>{item.unit}</li>
+            <button>Buy</button>
+            <button>Remove</button>
+        </ul>
+        ))}
+       </div>
+       </> 
+    )
+}
+
+export default GetShoppingList;
